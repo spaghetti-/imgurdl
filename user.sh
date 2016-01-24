23f255299c33502f3696e6b5e4a9209a2ee8aedd
Alex <dev@xrng.net>
2016-01-25 03:16:21 +0800

    retab

--- user.sh
+++ user.sh
@@ -5,7 +5,7 @@ url="http://$1.imgur.com/"
 
 for album in $(curl -s $url | grep -i "imgur.com/a/" | sed s/^.*imgur.com/imgur.com/ | sed s/\"\>// | sed s/^/http:\\/\\//)
 do
-	echo "Downloading album: $album"
-	./imgur.sh $album
+  echo "Downloading album: $album"
+  ./imgur.sh $album
 done
 
